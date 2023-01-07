a3e832d66bbafbe32c31247364da7cd7a3e688e0
navah <navah@LAPTOP-MJ7SJ5E2>
2023-01-07 18:57:28 -0500

    uploaded images and wrote img tags for each

--- ClientApp/src/components/movies.js
+++ ClientApp/src/components/movies.js
@@ -1,4 +1,8 @@
 ﻿import React, { Component } from 'react';
+import moonLight from './path/to/MoonlightSonataHBOdocmainpost5992.jpg';
+import ordinary from './path/to/NoOrdinaryHero.jpg';
+import audible from './path/to/Audible.jpg';
+import sound from './path/to/Sound-and-Fury-film-images-dc5b5fe4-82dc-4e7d-acc0-b08f6e1f1af.webp';
 
 export class Movies extends Component {
     static displayName = Movies.name;
@@ -6,7 +10,6 @@ export class Movies extends Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         return (
             <div>
@@ -17,22 +20,22 @@ export class Movies extends Component {
                     <li>
                         <h3>Moonlight Sonata: Deafness in Three Movements</h3>
                         <p>A personal memoir about a deaf boy growing up, his deaf grandfather aging, and Ludwig van Betthoven in the year he wrote his most iconic sonata.</p>
-                       {/* <img src="https://th.bing.com/th/id/OIP.tP61KqscnX_1d87QXcD4HwHaK9?pid=ImgDet&rs=1" alt="movie poster">*/}
+                        <img src={moonLight} alt="Moonlight Sonata Movie Poster" />
                     </li>
                     <li>
                         <h3>Audible</h3>
                         <p>Amaree is a deaf student at Maryland school for the deaf. Shaken by his friend committing suicide, he does everything in his memory. Amaree plays for the school football team who lose their first game in sixteen seasons. Amaree and the rest of the team spend a lot of time training for the upcoming homecoming game, his last game before he graduates. Watch Amaree as he plays football, builds relationships, and has fun with his friends.</p>
-{/*                        <img> src="https://ntvb.tmsimg.com/assets/p20101239_v_h8_aa.jpg?w=1280&h=720" alt="movie poster"</img>*/}
+                        <img src={audible} alt="audible netflix image" /> 
                     </li>
                     <li>
                         <h3>Sound and Fury</h3>
                         <p>Cousins Heather and Peter Artinian -- ages 6 and almost 2, respectively -- are deaf. Their condition could be changed by a cochlear implant, a device that stimulates hearing. The benefits are obvious, but this documentary focuses on why the children's families face a tough decision. There are concerns about how the device will change the recipient's relationship with deaf culture and whether there can ever be true connection between those who hear sounds and those who hear only silence.</p>
-                    {/*    <img>src="https://th.bing.com/th/id/OIP.VaDQcsH837RjTJJqhAz5NgHaKQ?pid=ImgDet&rs=1" alt="movie poster"</img>*/}
+                        <img src={sound} alt="Sound and Fury Movie Poster" />
                     </li>
                     <li>
                         <h3>No Ordinary Hero: The SuperDeafy Movie</h3>
                         <p>A deaf actor who plays a superhero on tv helps a young, deaf boy believe in himself when the two cross paths.</p>
-                    {/*    <img> src="https://th.bing.com/th/id/OIP.D9wwnZDxR_-Fz-35QRqeawHaOG?pid=ImgDet&rs=1" alt="movie poster"</img>*/}
+                        <img src={ordinary} alt="No Ordinary Hero: The SuperDeafy Movie" />
                     </li>
                 </ul>
 
