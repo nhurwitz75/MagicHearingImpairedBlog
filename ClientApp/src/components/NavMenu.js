af56612441a152dbb67c1cedae2aa67bce53c6ac
navah <navah@LAPTOP-MJ7SJ5E2>
2023-02-05 22:06:39 -0500

    hello

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -24,11 +24,11 @@ export class NavMenu extends Component {
     render() {
         return (
             <header>
-                <Navbar class="nav" className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
                     <NavbarBrand tag={Link} to="/">MagicHearingImpairedBlog</NavbarBrand>
                     <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
                     <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-                        <ul className="navbar-nav flex-grow">
+                        <ul class="list" className="navbar-nav flex-grow">
                             <NavItem>
                                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                             </NavItem>
--- ClientApp/src/components/movies.js
+++ ClientApp/src/components/movies.js
@@ -14,7 +14,7 @@ export class Movies extends Component {
     }
     render() {
         return (
-            <div>
+            <div class="box">
                 <h1>Movies and Books</h1>
 
                 <h5>Here is a list of movies and books that I found that I want to share with you that I think depicts hearing impairment well and can help make you more aware.</h5>
@@ -22,22 +22,22 @@ export class Movies extends Component {
                 <h2>Movies:</h2>
                 <ul>
                     <li>
-                        <a href="https://www.imdb.com/title/tt9135318/">Moonlight Sonata: Deafness in Three Movements</a>
+                        <a class="moon" href="https://www.imdb.com/title/tt9135318/">Moonlight Sonata: Deafness in Three Movements</a>
                         <p>A personal memoir about a deaf boy growing up, his deaf grandfather aging, and Ludwig van Betthoven in the year he wrote his most iconic sonata.</p>
                         {/*<img class="mooonlight" src={moonLight} alt="Moonlight Sonata Movie Poster" />*/}
                     </li>
                     <li>
-                        <a href="https://www.imdb.com/title/tt12771540/">Audible</a>
+                        <a class="audible" href="https://www.imdb.com/title/tt12771540/">Audible</a>
                         <p>Amaree is a deaf student at Maryland school for the deaf. Shaken by his friend committing suicide, he does everything in his memory. Amaree plays for the school football team who lose their first game in sixteen seasons. Amaree and the rest of the team spend a lot of time training for the upcoming homecoming game, his last game before he graduates. Watch Amaree as he plays football, builds relationships, and has fun with his friends.</p>
                         {/*<img class="audible" src={audible} alt="audible netflix image" />*/} 
                     </li>
                     <li>
-                        <a href="https://www.imdb.com/title/tt0240912/">Sound and Fury</a>
+                        <a class="sound" href="https://www.imdb.com/title/tt0240912/">Sound and Fury</a>
                         <p>Cousins Heather and Peter Artinian -- ages 6 and almost 2, respectively -- are deaf. Their condition could be changed by a cochlear implant, a device that stimulates hearing. The benefits are obvious, but this documentary focuses on why the children's families face a tough decision. There are concerns about how the device will change the recipient's relationship with deaf culture and whether there can ever be true connection between those who hear sounds and those who hear only silence.</p>
                         {/*<img class="sound" src={sound} alt="Sound and Fury Movie Poster" />*/}
                     </li>
                     <li>
-                        <a href="https://www.imdb.com/title/tt2907898/">No Ordinary Hero: The SuperDeafy Movie</a>
+                        <a class="ordinary" href="https://www.imdb.com/title/tt2907898/">No Ordinary Hero: The SuperDeafy Movie</a>
                         <p>A deaf actor who plays a superhero on tv helps a young, deaf boy believe in himself when the two cross paths.</p>
                         {/*<img class="ordinary" src={ordinary} alt="No Ordinary Hero: The SuperDeafy Movie" />*/}
                     </li>
