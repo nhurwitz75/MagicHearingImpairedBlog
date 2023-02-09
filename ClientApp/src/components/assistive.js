727cf7638c80e698632187fc5a48eedeb0c12b27
navah <navah@LAPTOP-MJ7SJ5E2>
2023-02-08 20:12:57 -0500

    Started the assistive hearing devices tab Added images to img folder in solution explorer

---
+++ ClientApp/src/components/assistive.js
@@ -0,0 +1,16 @@
+﻿import React { Component } from 'react';
+
+import './style/assistive.css';
+
+export class Assistive extends Component {
+    static displayName = assistive.name 
+
+    constructor(props) {
+        super(props);
+    }
+}
+    render() {
+        return (
+
+        )
+    }
